787b21a5d4fc4edf006cc471c8e4b0d314db4419
Anastasiia Lobova <anastasiia_lobova@epam.com>
2022-01-16 23:55:12 +0300

    add 6 homework

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://1u09qqfrv7.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://1u09qqfrv7.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://1u09qqfrv7.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://5kknalr9dk.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://1u09qqfrv7.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://1u09qqfrv7.execute-api.eu-west-1.amazonaws.com/dev',
 };
