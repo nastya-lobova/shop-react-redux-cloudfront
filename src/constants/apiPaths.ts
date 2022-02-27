ebcb8532262110a9c0f662d4ea24820a7b6e34a6
Anastasiia Lobova <anastasiia_lobova@epam.com>
2022-02-27 21:45:11 +0300

    add homework9

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://1u09qqfrv7.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://4ijdghl1dd.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://1u09qqfrv7.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://1u09qqfrv7.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://nastya-lobova-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
